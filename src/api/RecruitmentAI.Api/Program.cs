dff9ce8744fca6d5d0870c4bd037ddfdfbdbbb2c
ShawnTseng <shawntseng40@gmail.com>
2026-03-24 10:46:16 +0800

    fix: remove ResumeParserPlugin from kernelBuilder plugins

--- src/api/RecruitmentAI.Api/Program.cs
+++ src/api/RecruitmentAI.Api/Program.cs
@@ -70,7 +70,6 @@ if (!string.IsNullOrEmpty(aoaiEndpoint))
         kernelBuilder.Plugins.AddFromType<QaGeneratorPlugin>();
         kernelBuilder.Plugins.AddFromType<AnswerEvaluatorPlugin>();
         kernelBuilder.Plugins.AddFromType<ReportGeneratorPlugin>();
-        kernelBuilder.Plugins.AddFromType<ResumeParserPlugin>();
         return kernelBuilder.Build();
     });
 }
